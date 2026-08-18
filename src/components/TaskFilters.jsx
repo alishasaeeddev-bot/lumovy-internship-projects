519f2e136dc4e13b05a96c20a45bfab4a6111a10
Alisha Saeed <alishasaeed.dev@gmail.com>
2026-08-19 01:00:42 +0500

    Add React task management project

---
+++ src/components/TaskFilters.jsx
@@ -0,0 +1,12 @@
+function TaskFilters() {
+  return (
+    <section className="task-filters">
+      <h2>Task Filters</h2>
+      <button>All</button>
+      <button>Pending</button>
+      <button>Completed</button>
+    </section>
+  );
+}
+
+export default TaskFilters;
